34785385466835c8f39d8685ace5da4bb5def61a
ch1hyun <och2510@gmail.com>
2024-01-31 23:03:53 +0900

    JPA 강의 프로젝트 세팅

---
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,12 @@
+package jpabook.jpashop;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWi(SpringRunner.class)
+@SpringBootTest
+class MemberRepositoryTest {
+
+}
